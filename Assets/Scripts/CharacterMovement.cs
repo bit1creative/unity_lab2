d13115d612687cda97e7d330355c692546bbe2a6
bit1creative <bit1creative@gmail.com>
2020-10-14 18:11:47 +0300

    unity_lab3

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float speed;
+    public float walkSpeed = 6.0f;
+    public float sprintSpeed = 12.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20f;
+
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+    }
+
+    void Update()
+    {
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                speed = sprintSpeed;
+            }
+            else
+            {
+                speed = walkSpeed;
+            }
+
+            moveDirection *= speed;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+                //print("Jump");
+            }
+        
+        }
+        else
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), moveDirection.y, Input.GetAxis("Vertical"));
+            speed = walkSpeed;
+            moveDirection.x *= speed;
+            moveDirection.z *= speed;
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        // objects turns the way he goes
+        float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg;
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+        transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
